d6df900fd530b4511b71fa061d51dc98a6b174e2
Ranvijay Singh <ranvijay.singh@metacube.com>
2021-02-26 17:00:09 +0530

    maxMirror

--- src/TestingAndJUnit/ArrOperation.java
+++ src/TestingAndJUnit/ArrOperation.java
@@ -1,5 +1,32 @@
 package TestingAndJUnit;
 
 public class ArrOperation {
+	
+	
+	public int maxMirror(int[] arr){
+        int temp,max,count;
+        temp=max=count=0;
+        
+        for(int i=0; i<arr.length; ++i){
+        	count=0;
+        	temp=i;
+        	for(int j=arr.length-1; j>=temp; --j){
+        		  if(arr[temp] == arr[j]){
+                    count++;
+                    if(temp==j){
+                    	count++;
+                    }
+                    temp++;
+        		  }
+        		  else if(count > 0){
+        			  break;                
+        		  }
+        		  if(max < count){
+        			  max=count;
+        		  }
+        	}
+        }
 
+        return max;
+ }
 }
---
+++ src/TestingAndJUnit/Main.java
@@ -0,0 +1,15 @@
+package TestingAndJUnit;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ArrOperation AO=new ArrOperation();
+		int[] arr={7, 1, 4, 9, 7, 4, 1};
+		
+		System.out.println(AO.maxMirror(arr));
+
+	}
+
+}
