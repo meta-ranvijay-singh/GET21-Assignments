f1294b6b0d453f8486cedc19c332dce1cb96661c
Ranvijay Singh <ranvijay.singh@metacube.com>
2021-03-02 17:05:34 +0530

    Test cases

--- src/Recursion_Q3/NQueenTest.java
+++ src/Recursion_Q3/NQueenTest.java
@@ -6,12 +6,16 @@ import org.junit.Test;
 
 public class NQueenTest {
 
-	@Test
+	/**
+	 * JUnit test case for nQueen method
+	 */
+	@Test(expected = AssertionError.class)
 	public void testNQueen() {
 		NQueen queen=new NQueen();
 		assertTrue(queen.nQueen(new int[4][4], 0, 4));
 		assertTrue(queen.nQueen(new int[8][8], 0, 8));
 		assertFalse(queen.nQueen(new int[3][3], 0, 3));
+		assertFalse(queen.nQueen(new int[3][3], 0, -3));
 		
 	}
 
