e946cfa5541d7c185b8fef4ca4f83f6eadbd2d1f
Ranvijay Singh <ranvijay.singh@metacube.com>
2021-03-12 15:59:46 +0530

    question3

---
+++ src/Algorithm2_Q1/UndirectedGraph.java
@@ -0,0 +1,8 @@
+package Algorithm2_Q1;
+
+public interface UndirectedGraph {
+	public boolean isConnected();
+	public void reachable();
+	public void mst();
+	public int shortestPath(int a, int b);
+}
---
+++ src/Algorithm2_Q1/UndirectedWeightedGraph.java
@@ -0,0 +1,29 @@
+package Algorithm2_Q1;
+
+public class UndirectedWeightedGraph implements UndirectedGraph{
+
+	@Override
+	public boolean isConnected() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void reachable() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mst() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int shortestPath(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
