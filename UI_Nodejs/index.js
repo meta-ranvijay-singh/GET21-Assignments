b48c4a6a7c3d973726612cfa59b9be30994ee82d
Ranvijay Singh <ranvijay.singh@metacube.com>
2021-04-01 20:14:17 +0530

    UI_Nodejs

--- UI_Nodejs/index.js
+++ UI_Nodejs/index.js
@@ -1,9 +1,15 @@
 const http = require('http');
 const port = 3000;
+
+const mexp = require('math-expression-evaluator');
+
+let exp = "2+2/2"
+let value = mexp.eval(exp);
+
 const app = http.createServer(
     (req, res) => {
         res.setHeader('Content-Type', 'text/html');
-        res.write('<h1>Hello World!</h1>');
+        res.write(`<h1>${exp} = ${value}</h1>`);
         res.end();
         console.log(`server started at http://localhost:${port}`);
     }
